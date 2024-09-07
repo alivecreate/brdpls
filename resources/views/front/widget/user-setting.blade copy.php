a0600ba0adef0962c8790763baa1ea0d86e33cbf
Krishna Kahar <myalivecreate@gmail.com>
2024-09-07 09:17:53 +0530

    Cover pic and Profile Pic update fixed

--- resources/views/front/widget/user-setting.blade copy.php
+++ resources/views/front/widget/user-setting.blade copy.php
@@ -40,7 +40,7 @@
             
 
                 <div class="relative md:w-20 md:h-20 w-12 h-12 shrink-0"> 
-                    <label class="object-cover w-10 h-10">
+                    <label for="file" class="cursor-pointer">
                        
                         @if(getUserData()['image'])
                         
@@ -52,7 +52,13 @@
                         <img id="img" src="{{asset('front')}}/images/avatars/avatar-3.jpg"
                                     alt="Profile Picture" />
                         @endif
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="md:w-4 md:h-4 w-3 h-3 fill-white">
+                            <path d="M12 9a3.75 3.75 0 100 7.5A3.75 3.75 0 0012 9z"></path>
+                            <path fill-rule="evenodd" d="M9.344 3.071a49.52 49.52 0 015.312 0c.967.052 1.83.585 2.332 1.39l.821 1.317c.24.383.645.643 1.11.71.386.054.77.113 1.152.177 1.432.239 2.429 1.493 2.429 2.909V18a3 3 0 01-3 3h-15a3 3 0 01-3-3V9.574c0-1.416.997-2.67 2.429-2.909.382-.064.766-.123 1.151-.178a1.56 1.56 0 001.11-.71l.822-1.315a2.942 2.942 0 012.332-1.39zM6.75 12.75a5.25 5.25 0 1110.5 0 5.25 5.25 0 01-10.5 0zm12-1.5a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd"></path>
+                        </svg>
                         
+                        <input type="file" id="file" name='image'
+                         accept=".png, .webp, .jpg, .jpeg" />
                     </label>
                 </div>
 
@@ -60,8 +66,6 @@
                     
                 </div>
 
-                <input type="file" name='image'
-                         accept=".png, .webp, .jpg, .jpeg" />
                 <button type="submit" class="inline-flex items-center gap-1 py-1 pl-2.5 pr-3 rounded-full bg-slate-50 border-2 border-slate-100 dark:text-white dark:bg-slate-700" type="button" aria-haspopup="true" aria-expanded="false"> 
                     
                 <ion-icon onClick=disableButton(event) id="btnUpload" name="flash-outline" class="text-base duration-500 group-aria-expanded:rotate-180 md hydrated" role="img" aria-label="chevron down outline"></ion-icon> 
